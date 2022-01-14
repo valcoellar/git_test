33b423d9652d80e1870ef8eb3b9734f3ad079ece
valcoellar <valcoellar@gmail.com>
2022-01-14 13:47:46 -0600

    computerPlay()

---
+++ JS/ROCK_PAPER_SCISSORS/code.js
@@ -0,0 +1,12 @@
+// function called computerPlay that will randomly 
+// return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
+// We’ll use this function in the game to make the computer’s play.
+
+function computerPlay () {
+
+};
+
+
+
+
+console.log("Hello");
