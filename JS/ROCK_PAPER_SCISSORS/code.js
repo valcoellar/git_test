2f980047d12bc27a44faafeb7b5693bb1643e9bc
valcoellar <valcoellar@gmail.com>
2022-01-14 14:36:21 -0600

    computerPlay() Ready

--- JS/ROCK_PAPER_SCISSORS/code.js
+++ JS/ROCK_PAPER_SCISSORS/code.js
@@ -3,10 +3,18 @@
 // We’ll use this function in the game to make the computer’s play.
 
 function computerPlay () {
+ let cPlay = ""   
+ let dice =  Math.floor(Math.random() *  3 ) + 1; 
+
+if (dice = 1 ) {cPlay = "Rock"}
+if (dice = 2 ) {cPlay = "Paper"}
+if (dice = 3 ) {cPlay = "Scissors"}
+
+        
+return cPlay; 
 
 };
 
 
+console.log(computerPlay());
 
-
-console.log("Hello");
