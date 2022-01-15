4a3a00269fb7db0131b5c2e067a77b59c773e469
valcoellar <valcoellar@gmail.com>
2022-01-14 21:10:29 -0600

    computerPlay() Ok

--- JS/ROCK_PAPER_SCISSORS/code.js
+++ JS/ROCK_PAPER_SCISSORS/code.js
@@ -1,20 +1,26 @@
+
+
+function computerPlay () {
 // function called computerPlay that will randomly 
 // return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 // We’ll use this function in the game to make the computer’s play.
-
-function computerPlay () {
- let cPlay = ""   
+ let cPlay = " "   
  let dice =  Math.floor(Math.random() *  3 ) + 1; 
-
-if (dice = 1 ) {cPlay = "Rock"}
-if (dice = 2 ) {cPlay = "Paper"}
-if (dice = 3 ) {cPlay = "Scissors"}
-
-        
+switch (dice){
+    case 1:
+        cPlay = "Rock";
+    break;
+    case 2:
+        cPlay = "Paper";
+    break;
+    case 3:
+        cPlay = "Scissors";
+    break;
+}
 return cPlay; 
-
 };
 
 
 console.log(computerPlay());
+
 
