8cbefd26e44a84781f0aa7be424c96966e26b5b9
valcoellar <valcoellar@gmail.com>
2022-01-14 21:27:52 -0600

    Round Function

--- JS/ROCK_PAPER_SCISSORS/code.js
+++ JS/ROCK_PAPER_SCISSORS/code.js
@@ -21,6 +21,18 @@ return cPlay;
 };
 
 
-console.log(computerPlay());
+function Round (playerSelection, computerSelection){
+// Write a function that plays a single round of Rock Paper Scissors.
+// The function should take two parameters 
+// - the playerSelection and computerSelection 
+// - and then return a string that declares the winner 
+// of the round like so: "You Lose! Paper beats Rock"
+
+}
+
+
+
+
+// console.log(computerPlay());
 
 
