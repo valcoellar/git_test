202b733b80391ab2823f45df9253e20984ab0fd7
valcoellar <valcoellar@gmail.com>
2022-01-16 19:57:02 -0600

    ReverseString Test 1 & 2

--- JS/JS-Exercises/javascript-exercises/03_reverseString/reverseString.js
+++ JS/JS-Exercises/javascript-exercises/03_reverseString/reverseString.js
@@ -1,5 +1,16 @@
 const reverseString = function(str) {
+// Pretty simple, write a function called `reverseString` that returns its input, reversed!
+// reverseString('hello there') // returns 'ereht olleh'
 
+
+let CountChars = str.length;
+let ReversedString = "";
+
+for (let i = CountChars ; i > 0; i--){
+    ReversedString = ReversedString +  str.charAt(i-1);         
+}
+
+return ReversedString;
 };
 
 // Do not edit below this line
