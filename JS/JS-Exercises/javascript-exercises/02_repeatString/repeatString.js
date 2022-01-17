a3011cd167e33225f84c2bc89f95cd5f98260447
valcoellar <valcoellar@gmail.com>
2022-01-16 19:21:50 -0600

    repeatString test1

--- JS/JS-Exercises/javascript-exercises/02_repeatString/repeatString.js
+++ JS/JS-Exercises/javascript-exercises/02_repeatString/repeatString.js
@@ -1,4 +1,11 @@
-const repeatString = function() {
+const repeatString = function(cString, Nnum) {
+// This function will take two arguments, `string` and `num`.
+let Srepeats = ""
+for (let i = 0; i < Nnum; i++){
+Srepeats = Srepeats +cString;
+}
+
+return Srepeats;
 
 };
 
