ffd56ae81224a745a4a71ebafe4ded718db35693
valcoellar <valcoellar@gmail.com>
2022-01-23 16:47:24 -0600

    sumAll

--- JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
+++ JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(Nstart, Nend) {
+    // Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
+    // expect(sumAll(1, 4)).toEqual(10);
+
+let Nresutl = 0;
+for (let i = Nstart; i <= Nend; i++){
+    Nresutl =  i;
+    
+    
+}
+
+return Nresutl;
 
 };
 
