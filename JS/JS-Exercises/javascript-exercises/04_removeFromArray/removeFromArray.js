204b13ccc7aa2668a3d660632101a50fb4bb0bd9
valcoellar <valcoellar@gmail.com>
2022-01-22 21:28:32 -0600

    RemoveFromArray

--- JS/JS-Exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ JS/JS-Exercises/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(BaseArray, ItemToRemove) {
+// Implement a function that takes an array and some other arguments then removes the other arguments from that array:
+
+// ```javascript
+//removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+//```
+
+let SelectedItem = BaseArray.indexOf(ItemToRemove);
+
+return SelectedItem.splice(0,SelectedItem);
+
+
+
 
 };
 
