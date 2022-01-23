33e666f1978ce34fccfbb8abfa099270a764cb82
valcoellar <valcoellar@gmail.com>
2022-01-23 16:51:58 -0600

    sumAll 1 passed

--- JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
+++ JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -2,14 +2,14 @@ const sumAll = function(Nstart, Nend) {
     // Implement a function that takes 2 integers and returns the sum of every number between(and including) them:
     // expect(sumAll(1, 4)).toEqual(10);
 
-let Nresutl = 0;
+let Nresult = 0;
 for (let i = Nstart; i <= Nend; i++){
-    Nresutl =  i;
+    Nresult = Nresult + i;
     
     
 }
 
-return Nresutl;
+return Nresult;
 
 };
 
