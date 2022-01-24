e1bfeb1d0ee8ebecb74f7061aacb6bda29b5ed92
valcoellar <valcoellar@gmail.com>
2022-01-23 20:50:48 -0600

    sumAll 6 passed

--- JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
+++ JS/JS-Exercises/javascript-exercises/05_sumAll/sumAll.js
@@ -11,18 +11,13 @@ if (Nstart > Nend) {  // swapping the honey !
  if ((Nstart < 0) || (Nend < 0)) {return "ERROR";} // negatives
  if ((typeof Nstart != "number") || (typeof Nend != "number")) {return "ERROR";} // non numbers parms
  
-
     let Nresult = 0;
         for (let i = Nstart; i <= Nend; i++){
             Nresult = Nresult + i;
                                             }
 
     return Nresult;
-                    
-
-
-
-
+    
 };
 
 // Do not edit below this line
