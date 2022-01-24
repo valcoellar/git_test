3a3cca7f8c582b19d548fd7280d212ba7f36de0d
valcoellar <valcoellar@gmail.com>
2022-01-23 22:10:17 -0600

    leapYears 1 passed

--- JS/JS-Exercises/javascript-exercises/06_leapYears/leapYears.js
+++ JS/JS-Exercises/javascript-exercises/06_leapYears/leapYears.js
@@ -1,4 +1,25 @@
-const leapYears = function() {
+const leapYears = function(InputYear) {
+
+    // Create a function that determines whether or not a given year is a leap year. Leap years are determined by the following rules:
+
+    // > Leap years are years divisible by four (like 1984 and 2004). 
+    //However, years divisible by 100 are not leap years (such as 1800 and 1900)
+    //unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years).
+    // (Yes, it's all pretty confusing)
+
+    // leapYears(2000) // is a leap year: returns true 
+    // leapYears(1985) // is not a leap year: returns false
+
+    // all 0 is leap  true
+
+let Nfirst = InputYear % 4;
+let Nsecond = InputYear % 100 ;
+let Nthird = InputYear % 400 ;
+
+if (((Nfirst = 0) && (Nsecond = 0)) && (Nthird = 0)) {return false;} else {return true;}
+
+
+
 
 };
 
