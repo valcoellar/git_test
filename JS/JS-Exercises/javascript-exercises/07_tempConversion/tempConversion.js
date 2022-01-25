244708f31614541378738a8d52984ce0ac7c7f3b
valcoellar <valcoellar@gmail.com>
2022-01-24 21:46:26 -0600

    tempConversion 1 passed

--- JS/JS-Exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ JS/JS-Exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,17 +1,23 @@
-const ftoc = function() {
+const ftoc = function(Itemperature) {
   // Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
   //```
   //ftoc(32) // fahrenheit to celsius, should return 0
   //ctof(0) // celsius to fahrenheit, should return 32
   //```
 
+// (32 °F − 32) × 5/9 = 0 °C
 
+let Result_Temp = ((Itemperature - 32) * 5/9);
 
+return Result_Temp;
 };
 
 const ctof = function() {
 
 };
+
+
+
 
 // Do not edit below this line
 module.exports = {
