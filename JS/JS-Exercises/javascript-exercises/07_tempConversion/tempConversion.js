a89306177d797a4fdb564598b81357200b207890
valcoellar <valcoellar@gmail.com>
2022-01-24 21:32:42 -0600

    tempConversion

--- JS/JS-Exercises/javascript-exercises/07_tempConversion/tempConversion.js
+++ JS/JS-Exercises/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,4 +1,11 @@
 const ftoc = function() {
+  // Write two functions that convert temperatures from Fahrenheit to Celsius, and vice versa:
+  //```
+  //ftoc(32) // fahrenheit to celsius, should return 0
+  //ctof(0) // celsius to fahrenheit, should return 32
+  //```
+
+
 
 };
 
