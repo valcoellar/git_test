f119b29c15711abff90877f1da43636fdb079c0c
valcoellar <valcoellar@gmail.com>
2022-01-30 21:56:48 -0600

    div

--- JS/JS-Exercises/DOM_exercises/script.js
+++ JS/JS-Exercises/DOM_exercises/script.js
@@ -27,3 +27,15 @@ const content = document.createElement('div');
 			content3.style.color = "blue";
 	container.appendChild(content3);
 	
+// a <div> with a black border and pink background color with the 
+// following elements inside of it:
+// another <h1> that says “I’m in a div”
+// a <p> that says “ME TOO!”
+// Hint for this one: after creating the <div> with createElement,
+//append the <h1> and <p> to it before adding it to the container.	
+
+
+
+
+
+
