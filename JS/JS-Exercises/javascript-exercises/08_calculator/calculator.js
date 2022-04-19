d4cd5f8ceec1d964ddbc62157ed46d306ed16356
valcoellar <valcoellar@gmail.com>
2022-04-18 21:12:09 -0500

    calculator

--- JS/JS-Exercises/javascript-exercises/08_calculator/calculator.js
+++ JS/JS-Exercises/javascript-exercises/08_calculator/calculator.js
@@ -1,13 +1,47 @@
 const add = function() {
-	
-};
+
+  if (arguments.length === 2){
+
+    if ((arguments[0] && arguments[1] ) === 0) { 
+            var ADD_result = 0;
+            return ADD_result;
+                    }  else {
+            var ADD_result = (arguments[0] + arguments[1] );
+            return ADD_result;
+                            }
+             
+                                                };
+  
+// ------------------------------------------
+                                                  }
+
+
+
 
 const subtract = function() {
 	
+  if (arguments.length === 2){
+
+    if ((arguments[0] && arguments[1] ) === 0) { 
+            var ADD_result = 0;
+            return ADD_result;
+                    }  else {
+            var ADD_result = (arguments[0] - arguments[1] );
+            return ADD_result;
+                            }
+             
+                                                };
+
 };
 
 const sum = function() {
-	
+  //computes the sum of an empty array
+  let args = arguments.valueOf();
+  let S_args = args[0].toString();
+  let N_args = parseInt(S_args);
+  if (isNaN(N_args)) N_args = 0;
+  return N_args ;  
+  	
 };
 
 const multiply = function() {
