17d92f382d6c16855b3531c0aa4ff04deafd2e2d
valcoellar <valcoellar@gmail.com>
2022-04-26 10:00:37 -0500

    pali

--- JS/JS-Exercises/javascript-exercises/09_palindromes/palindromes.js
+++ JS/JS-Exercises/javascript-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
 const palindromes = function () {
-
+        // palindromes
+        // ## some palindromes:
+        //  - A car, a man, a maraca.
+        //  - Rats live on no evil star.
+        //  - Lid off a daffodil.
+        //  - Animal loots foliated detail of stool lamina.
+        // - A nut for a jar of tuna.
+       
+       // 1-normalize data
+       // (all to lowcase)
+       // (quit all spaces) <<< Result string  //and , . 
+       // 2-mirror all string
+       // 3-compare S_ResultString with S_MirrorString
+       // return boolean true or false
+       
+       let S_string = arguments[0].toLowerCase();
+       let S_ResultString = S_string.replace(/\s/g, '');
+       S_ResultString = S_ResultString.replace(/[^a-z]+/g, "");
+       let S_MirrorString = S_ResultString.split("").reverse().join("");
+       
+       if (S_ResultString === S_MirrorString) {
+         let Bol_Resutl = true;
+       return Bol_Resutl;
+       } else {
+         let Bol_Resutl = false;
+       return Bol_Resutl;
+       } 
+        
+         // End Function 
+        
 };
 
 // Do not edit below this line
