d630588ae192db3dbf840d4b12571b0b5dde60dd
valcoellar <valcoellar@gmail.com>
2022-05-22 07:32:02 -0500

    22052022-0731am

--- Calculator/code.js
+++ Calculator/code.js
@@ -1,1 +1,12 @@
 //code js
+
+
+// function on clear display and puts 0
+
+const Power_Button = document.querySelector(".on_off");
+
+Power_Button.addEventListener("click",function PowerOn(){
+    console.log("click it");
+}
+ );
+
