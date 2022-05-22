a760bb4fa080392a881ed365a26c0e049ff3a33f
valcoellar <valcoellar@gmail.com>
2022-05-22 07:45:01 -0500

    22052022-0744am

--- Calculator/code.js
+++ Calculator/code.js
@@ -1,12 +1,29 @@
 //code js
 
+// *------------------- Functions ----------------------
+function PowerOn() {
+    // Clear display and puts 0
+    const Display_Screen = document.querySelector(".N_Result");
+    Display_Screen.textContent = "0";
+    // console.log("click it");
+};
 
-// function on clear display and puts 0
+function All_Clear (){
+    //Clear all variables and stand ready for next
+};
+
+function Clear_Display (){
+    //Clear only the display
+};
+
+
+
+
+//----------------------Listeners ----------------------
 
 const Power_Button = document.querySelector(".on_off");
+Power_Button.addEventListener("click",PowerOn);
 
-Power_Button.addEventListener("click",function PowerOn(){
-    console.log("click it");
-}
- );
 
+
+// to-do: add input functions 
